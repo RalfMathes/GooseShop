bf31ac2035bd409c56c7587884ce85f33abb59cc
RalfMathes <ralf.mathes@tutanota.com>
2023-05-10 19:45:08 +0200

    Initialize router with express

--- backend/modules/router.js
+++ backend/modules/router.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+module.exports = router;
