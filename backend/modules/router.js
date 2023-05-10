4983a9d631416b6513b0092c31ee9dd5f78a2b39
RalfMathes <ralf.mathes@tutanota.com>
2023-05-10 20:01:36 +0200

    Add routes with correlating function calls to router

--- backend/modules/router.js
+++ backend/modules/router.js
@@ -4,7 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
 
+const database = require("./db").default;
+
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
+
+router.get("/collection", database.getAllCollections);
+router.get("/collection/:collectionId", database.getCollectionById);
+router.get("/category", database.getAllCategories);
+router.get("/category/:categoryId", database.getCategoryById);
+router.get("/item/:itemId", database.getItemById);
 
 module.exports = router;
