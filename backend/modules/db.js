d1340d5c407e6890a084685944804bbaae8dcd0c
RalfMathes <ralf.mathes@tutanota.com>
2023-05-10 20:01:13 +0200

    Implement and export methods with mock return value

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const getAllCollections = (request, response) => {
+  return response.status(200).json("getAllCollections");
+};
+
+const getCollectionById = (request, response) => {
+  const collectionId = parseInt(request.params.collectionId);
+  return response.status(200).json("getCollectionById " + collectionId);
+};
+
+const getAllCategories = (request, response) => {
+  return response.status(200).json("getAllCategories");
+};
+
+const getCategoryById = (request, response) => {
+  const categoryId = parseInt(request.params.categoryId);
+  return response.status(200).json("getCategoryById " + categoryId);
+};
+
+const getItemById = (request, response) => {
+  const itemId = parseInt(request.params.itemId);
+  return response.status(200).json("getItemById " + itemId);
+};
+
+module.exports = {
+  getAllCollections,
+  getCollectionById,
+  getAllCategories,
+  getCategoryById,
+  getItemById,
+};
