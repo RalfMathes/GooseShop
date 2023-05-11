12c60a509584175230bdc4ed6eb98761eab1aca3
RalfMathes <ralf.mathes@tutanota.com>
2023-05-11 16:52:03 +0200

    Add Menu to navbar for mobile view

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,27 +1,80 @@
 import {
   AppBar,
+  Avatar,
+  Box,
   Button,
   Container,
+  IconButton,
+  Menu,
+  MenuItem,
   Toolbar,
+  Tooltip,
   Typography,
   useTheme,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { Link } from "react-router-dom";
+import * as React from "react";
+
+const pages = ["Home", "Collections", "Categories"];
+const paths = ["/", "/collection", "/category"];
 
 const Navbar = () => {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("sm"));
   return matches ? (
     <AppBar position="sticky">
       <Container>
         <Toolbar>
-          <Button>
-            <Link style={{ textDecoration: "none", color: "white" }} to="/cart">
-              <MenuIcon />
-            </Link>
-          </Button>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <MenuIcon style={{ color: "white" }} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {pages.map((page, index) => (
+                <MenuItem key={page}>
+                  <Typography textAlign="center">
+                    <Link
+                      style={{ textDecoration: "none", color: "black" }}
+                      to={paths[index]}
+                    >
+                      {page}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
