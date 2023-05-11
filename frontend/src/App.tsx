a03915910e43d3fd2fa4a75e73fd1a5fb98f77ee
RalfMathes <ralf.mathes@tutanota.com>
2023-05-11 16:43:58 +0200

    Add routing to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Home from "./pages/home";
+import Collection from "./pages/Collection";
+import Cart from "./pages/Cart";
+import Category from "./pages/Category";
+import Item from "./pages/Item";
+
 const App = () => {
-  return <h1>Filler</h1>;
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/item" element={<Item />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Container>
+    </>
+  );
 };
 
 export default App;
