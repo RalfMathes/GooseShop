132d7c62c020899f6027798773be1f25dff01391
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 20:13:18 +0200

    Add storeItems slice

---
+++ frontend/src/redux/slices/storeItems/storeItems.ts
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { RootState } from "../../store";
+import axios from "axios";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+export interface StoreItemsState {
+  loading: boolean;
+  items: StoreItemProps[];
+  error: string;
+}
+
+const initialState: StoreItemsState = {
+  loading: false,
+  items: [],
+  error: "",
+};
+
+const fetchStoreItems = createAsyncThunk<StoreItemProps[]>(
+  "storeItems/fetchStoreItems",
+  async () => {
+    const response = await axios.get("http://localhost:3000/items");
+    return response.data;
+  }
+);
+
+const storeItemsSlice = createSlice({
+  name: "storeItems",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchStoreItems.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchStoreItems.fulfilled, (state, action) => {
+      state.loading = false;
+      state.items = action.payload;
+      state.error = "";
+    });
+    builder.addCase(fetchStoreItems.rejected, (state, action) => {
+      state.loading = false;
+      state.items = [];
+      state.error = action.error.message ?? "";
+    });
+  },
+});
+
+export default storeItemsSlice.reducer;
+export const selectShoppingCartCount = (state: RootState) =>
+  state.storeItems.items;
+export { fetchStoreItems };
