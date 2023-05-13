63946bc205f693aca8e521ce0c115f0d240cff57
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 10:31:55 +0200

    Add shopping cart component

---
+++ frontend/src/components/ShoppingCart.tsx
@@ -0,0 +1,32 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { makeStyles } from "tss-react/mui";
+import { Badge, IconButton } from "@mui/material";
+import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const ShoppingCart = () => {
+  const { classes } = useStyles();
+  const shoppingCartCount = useSelector<RootState, number>(
+    (state) => state.shoppingCart.count
+  );
+
+  return (
+    <IconButton>
+      <Link className={classes.shoppingCartLink} to="/cart">
+        <Badge badgeContent={shoppingCartCount} color="error">
+          <ShoppingCartIcon />
+        </Badge>
+      </Link>
+    </IconButton>
+  );
+};
+
+const useStyles = makeStyles()(() => ({
+  shoppingCartLink: {
+    textDecoration: "none",
+    color: "white",
+  },
+}));
+
+export default ShoppingCart;
