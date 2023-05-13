4ca05fa17d9381ba227dff0f3d107f7722f04243
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 20:13:35 +0200

    Add store items slice to reducers

--- frontend/src/redux/reducers.ts
+++ frontend/src/redux/reducers.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import anchorElementReducer from "../redux/slices/anchorElementMenu/anchorElementMenu";
 import shoppingCart from "./slices/shoppingCart/shoppingCart";
+import storeItems from "./slices/storeItems/storeItems";
 
 const rootReducer = combineReducers({
   anchorElementReducer,
   shoppingCart,
+  storeItems,
 });
 
 export default rootReducer;
