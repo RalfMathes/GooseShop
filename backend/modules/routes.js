70fc1c1fc5c6fe70223760430a52c0886eb88f06
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 19:49:41 +0200

    Add items route, rename routes and add cors policy workaround

--- backend/modules/routes.js
+++ backend/modules/routes.js
@@ -3,16 +3,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
+var cors = require('cors');
 
 const database = require("./db");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/collection", database.getAllCollections);
-router.get("/collection/:collectionId", database.getCollectionById);
-router.get("/category", database.getAllCategories);
-router.get("/category/:categoryId", database.getCategoryById);
-router.get("/item/:itemId", database.getItemById);
+router.use(cors({
+  origin: '*',
+  exposedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept' ],
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+}));
+
+router.get("/collections", database.getAllCollections);
+router.get("/collections/:collectionId", database.getCollectionById);
+router.get("/categories", database.getAllCategories);
+router.get("/categories/:categoryId", database.getCategoryById);
+router.get("/items", database.getAllItems);
+router.get("/items/:itemId", database.getItemById);
 
 module.exports = router;
