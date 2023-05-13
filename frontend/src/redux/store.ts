57f91df962f74d657b22738f25e80f18ddd3f860
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 15:08:44 +0200

    Add persist middleware to redux store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,20 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
-import anchorElementReducer from "../redux/slices/anchorElementMenu/anchorElementMenu";
-import shoppingCartReducer from "./slices/shoppingCart/shoppingCart";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    anchorElement: anchorElementReducer,
-    shoppingCart: shoppingCartReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Remove this if you want to be taken seriously...
     }),
 });
 
+const persistor = persistStore(store);
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
+export { persistor };
