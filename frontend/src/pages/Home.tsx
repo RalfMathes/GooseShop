cddcd11f369d3a6dc9d286afc05db3c2bdbd3b2c
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 20:14:24 +0200

    Replace local mock with fetched items in Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,40 +1,32 @@
 import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import StoreItem from "../components/StoreItem";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStoreItems } from "../redux/slices/storeItems/storeItems";
+import { AppDispatch, RootState } from "../redux/store";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
 
 const Home = () => {
-  const itemsJson = [
-    {
-      id: 1,
-      name: "knife",
-      price: 15.99,
-      imgUrl: "/static/goose.svg",
-    },
-    {
-      id: 2,
-      name: "goose plush",
-      price: 17.99,
-      imgUrl: "/static/goose.svg",
-    },
-    {
-      id: 3,
-      name: "goose energy drink",
-      price: 3,
-      imgUrl: "/static/goose.svg",
-    },
-    {
-      id: 4,
-      name: "goose turbo",
-      price: 115.99,
-      imgUrl: "/static/goose.svg",
-    },
-  ];
+  const storeItems: StoreItemProps[] = useSelector<RootState, StoreItemProps[]>(
+    (state) => state.storeItems.items
+  );
+  const dispatch = useDispatch<AppDispatch>();
 
+  useEffect(() => {
+    dispatch(fetchStoreItems());
+  }, []);
   return (
     <>
       <Typography variant="h1">Home</Typography>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {itemsJson.map((item) => (
+        {storeItems.map((item) => (
           <Grid key={item.id} item xs={12} sm={6}>
             <StoreItem {...item} />
           </Grid>
