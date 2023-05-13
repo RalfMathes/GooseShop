401d3d32a98b947428f13268270a92415081062d
RalfMathes <ralf.mathes@tutanota.com>
2023-05-13 19:49:01 +0200

    Add mock data and extra route in db.js

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -1,5 +1,32 @@
 "use strict";
 
+const itemsJson = [
+  {
+    id: 1,
+    name: "knife",
+    price: 15.99,
+    imgUrl: "/static/goose.svg",
+  },
+  {
+    id: 2,
+    name: "goose plush",
+    price: 17.99,
+    imgUrl: "/static/goose.svg",
+  },
+  {
+    id: 3,
+    name: "goose energy drink",
+    price: 3,
+    imgUrl: "/static/goose.svg",
+  },
+  {
+    id: 4,
+    name: "goose turbo",
+    price: 115.99,
+    imgUrl: "/static/goose.svg",
+  },
+];
+
 const getAllCollections = (request, response) => {
   return response.status(200).json("getAllCollections");
 };
@@ -18,6 +45,10 @@ const getCategoryById = (request, response) => {
   return response.status(200).json("getCategoryById " + categoryId);
 };
 
+const getAllItems = (request, response) => {
+  return response.status(200).json(itemsJson);
+};
+
 const getItemById = (request, response) => {
   const itemId = parseInt(request.params.itemId);
   return response.status(200).json("getItemById " + itemId);
@@ -28,5 +59,6 @@ module.exports = {
   getCollectionById,
   getAllCategories,
   getCategoryById,
+  getAllItems,
   getItemById,
 };
