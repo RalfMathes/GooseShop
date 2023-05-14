5f584ae2b89a8c357bef55102c376cf9e81495d7
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 16:29:05 +0200

    Give Cart page styling

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,6 +1,8 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, useTheme } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
+import { makeStyles } from "tss-react/mui";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import CartItem from "../components/CartItem";
 
 type CartItemProps = {
@@ -9,13 +11,22 @@ type CartItemProps = {
 };
 
 const Cart = () => {
+  const { classes } = useStyles();
   const items = useSelector<RootState, CartItemProps[]>(
     (state) => state.shoppingCartReducer.items
   );
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <>
-      <Typography variant="h2">Cart</Typography>
+      {matches ? (
+        <Box className={classes.titleBoxMobile}>
+          <Typography variant="h3">Cart</Typography>
+        </Box>
+      ) : (
+        <Typography variant="h3">Cart</Typography>
+      )}
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {items.map((item) => (
           <Grid key={item.id} item xs={12} sm={12}>
@@ -28,3 +39,11 @@ const Cart = () => {
 };
 
 export default Cart;
+
+const useStyles = makeStyles()(() => ({
+  titleBoxMobile: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "primary.bright",
+  },
+}));
