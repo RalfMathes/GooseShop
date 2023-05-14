d0e7906e471bc8717c7c8950c06f1807db51fd31
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 17:23:12 +0200

    Rework Home page to fit requirements

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,79 @@
-import { Box, Grid } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Grid,
+  Stack,
+} from "@mui/material";
 import Typography from "@mui/material/Typography";
-import StoreItem from "../components/StoreItem";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchStoreItems } from "../redux/slices/storeItems/storeItems";
 import { AppDispatch, RootState } from "../redux/store";
 import { makeStyles } from "tss-react/mui";
+import { fetchCategories } from "../redux/slices/categories/categories";
+import { fetchCollections } from "../redux/slices/collections/collections";
+import StoreCategory from "../components/StoreCategory";
+import StoreCollection from "../components/StoreCollection";
 
-type StoreItemProps = {
+type CategoryProps = {
   id: number;
-  categoryId: number;
-  collectionId: number;
   name: string;
-  price: number;
+  imgUrl: string;
+};
+
+type CollectionProps = {
+  id: number;
+  name: string;
   imgUrl: string;
 };
 
 const Home = () => {
   const { classes } = useStyles();
-  const storeItems: StoreItemProps[] = useSelector<RootState, StoreItemProps[]>(
-    (state) => state.storeItemsReducer.items
+  const collections: CollectionProps[] = useSelector<
+    RootState,
+    CollectionProps[]
+  >((state) => state.collectionsReducer.collections);
+  const categories: CategoryProps[] = useSelector<RootState, CategoryProps[]>(
+    (state) => state.categoriesReducer.categories
   );
   const appDispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    appDispatch(fetchStoreItems());
+    appDispatch(fetchCategories());
+    appDispatch(fetchCollections());
   }, []);
+
   return (
     <>
       <Box className={classes.titleBoxMobile}>
         <Typography variant="h3">Home</Typography>
       </Box>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {storeItems.map((item) => (
-          <Grid key={item.id} item xs={12} sm={6}>
-            <StoreItem {...item} />
-          </Grid>
-        ))}
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography variant="h4" className={classes.contentTitle}>
+            Categories
+          </Typography>
+          <Stack>
+            {categories.map((category) => (
+              <Box key={category.id}>
+                <StoreCategory {...category} />
+              </Box>
+            ))}
+          </Stack>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h4" className={classes.contentTitle}>
+            Collections
+          </Typography>
+          <Stack>
+            {collections.map((collection) => (
+              <Box key={collection.id}>
+                <StoreCollection {...collection} />
+              </Box>
+            ))}
+          </Stack>
+        </Grid>
       </Grid>
     </>
   );
@@ -45,6 +82,10 @@ const Home = () => {
 export default Home;
 
 const useStyles = makeStyles()(() => ({
+  contentTitle: {
+    display: "flex",
+    justifyContent: "center",
+  },
   titleBoxMobile: {
     display: "flex",
     justifyContent: "center",
