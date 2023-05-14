de7dab977ae0eca77892fc5f58c260e0984f7a68
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 16:30:04 +0200

    Overhaul Routing in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,48 @@
 import { Routes, Route } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Home from "./pages/Home";
-import Collection from "./pages/Collection";
+import Collections from "./pages/Collections";
 import Cart from "./pages/Cart";
-import Category from "./pages/Category";
+import Categories from "./pages/Categories";
 import Item from "./pages/Item";
 import Navbar from "./components/Navbar";
+import { CssBaseline, createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import Category from "./pages/Category";
+import Collection from "./pages/Collection";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fb8b17",
+    },
+    secondary: {
+      main: "#ff7500",
+    },
+    background: {
+      default: "#fff8eb",
+    },
+  },
+});
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/collection" element={<Collection />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/item" element={<Item />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collections />} />
+            <Route path="/collection/:collectionId" element={<Collection />} />
+            <Route path="/category" element={<Categories />} />
+            <Route path="/category/:categoryId" element={<Category />} />
+            <Route path="/item/:itemId" element={<Item />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
