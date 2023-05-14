f2077dd6de1592128b669b624b078e83bc25f036
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 17:25:04 +0200

    Remove superfluous anchorElement slice

--- frontend/src/redux/reducers.ts
+++ frontend/src/redux/reducers.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
-import anchorElementReducer from "../redux/slices/anchorElementMenu/anchorElementMenu";
 import shoppingCartReducer from "./slices/shoppingCart/shoppingCart";
 import storeItemsReducer from "./slices/storeItems/storeItems";
 import categoriesReducer from "./slices/categories/categories";
 import collectionsReducer from "./slices/collections/collections";
 
 const rootReducer = combineReducers({
-  anchorElementReducer,
   shoppingCartReducer,
   storeItemsReducer,
   categoriesReducer,
