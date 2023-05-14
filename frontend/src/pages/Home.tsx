c10c0e2ea620561bda19e9f0745eacf3e604abc8
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 16:27:35 +0200

    Give Home page styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import StoreItem from "../components/StoreItem";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchStoreItems } from "../redux/slices/storeItems/storeItems";
 import { AppDispatch, RootState } from "../redux/store";
+import { makeStyles } from "tss-react/mui";
 
 type StoreItemProps = {
   id: number;
@@ -14,6 +15,7 @@ type StoreItemProps = {
 };
 
 const Home = () => {
+  const { classes } = useStyles();
   const storeItems: StoreItemProps[] = useSelector<RootState, StoreItemProps[]>(
     (state) => state.storeItemsReducer.items
   );
@@ -24,7 +26,9 @@ const Home = () => {
   }, []);
   return (
     <>
-      <Typography variant="h1">Home</Typography>
+      <Box className={classes.titleBoxMobile}>
+        <Typography variant="h3">Home</Typography>
+      </Box>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {storeItems.map((item) => (
           <Grid key={item.id} item xs={12} sm={6}>
@@ -37,3 +41,11 @@ const Home = () => {
 };
 
 export default Home;
+
+const useStyles = makeStyles()(() => ({
+  titleBoxMobile: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "primary.bright",
+  },
+}));
