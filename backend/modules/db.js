1bd8dd6dd07b9ed45daf12e32e4936f3190e4772
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 16:06:25 +0200

    Add collections and categories to mock API

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -3,32 +3,94 @@
 const itemsJson = [
   {
     id: 1,
+    categoryId: 1,
+    collectionId: 1,
     name: "knife",
     price: 15.99,
     imgUrl: "/static/goose.svg",
   },
   {
     id: 2,
+    categoryId: 2,
+    collectionId: 1,
     name: "goose plush",
     price: 17.99,
     imgUrl: "/static/goose.svg",
   },
   {
     id: 3,
+    categoryId: 2,
+    collectionId: 1,
     name: "goose energy drink",
     price: 3,
     imgUrl: "/static/goose.svg",
   },
   {
     id: 4,
+    categoryId: 2,
+    collectionId: 1,
     name: "goose turbo",
     price: 115.99,
     imgUrl: "/static/goose.svg",
   },
+  {
+    id: 5,
+    categoryId: 3,
+    collectionId: 2,
+    name: "mechanical keyboard",
+    price: 220.99,
+    imgUrl: "/static/keyboard.jpeg",
+  },
+  {
+    id: 6,
+    categoryId: 3,
+    collectionId: 2,
+    name: "keyboard switches",
+    price: 6.99,
+    imgUrl: "/static/switches.webp",
+  },
+  {
+    id: 7,
+    categoryId: 3,
+    collectionId: 2,
+    name: "keycaps",
+    price: 12.99,
+    imgUrl: "/static/keycaps.webp",
+  },
 ];
 
+const categoriesJson = [
+  {
+    id: 1,
+    name: "knives",
+    imgUrl: "/static/knives.jpeg",
+  },
+  {
+    id: 2,
+    name: "goose",
+    imgUrl: "/static/geese.webp",
+  },
+  {
+    id: 3,
+    name: "keyboards",
+    imgUrl: "/static/keyboards.webp",
+  },
+]
+const collectionsJson = [
+  {
+    id: 1,
+    name: "goose",
+    imgUrl: "/static/goose_collection.webp",
+  },
+  {
+    id: 2,
+    name: "not-goose",
+    imgUrl: "/static/not_goose_collection.webp",
+  },
+]
+
 const getAllCollections = (request, response) => {
-  return response.status(200).json("getAllCollections");
+  return response.status(200).json(collectionsJson);
 };
 
 const getCollectionById = (request, response) => {
@@ -37,7 +99,7 @@ const getCollectionById = (request, response) => {
 };
 
 const getAllCategories = (request, response) => {
-  return response.status(200).json("getAllCategories");
+  return response.status(200).json(categoriesJson);
 };
 
 const getCategoryById = (request, response) => {
