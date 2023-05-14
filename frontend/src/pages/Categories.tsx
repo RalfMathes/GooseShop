6fb20295eb7c4bbd7943b95f1a462a89eeb66670
RalfMathes <ralf.mathes@tutanota.com>
2023-05-14 16:26:12 +0200

    Add Categories page

---
+++ frontend/src/pages/Categories.tsx
@@ -0,0 +1,50 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCategories } from "../redux/slices/categories/categories";
+import { AppDispatch, RootState } from "../redux/store";
+import { makeStyles } from "tss-react/mui";
+import StoreCategory from "../components/StoreCategory";
+
+type CategoriesProps = {
+  id: number;
+  name: string;
+  imgUrl: string;
+};
+
+const Categories = () => {
+  const { classes } = useStyles();
+  const categories: CategoriesProps[] = useSelector<
+    RootState,
+    CategoriesProps[]
+  >((state) => state.categoriesReducer.categories);
+  const appDispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    appDispatch(fetchCategories());
+  }, []);
+  return (
+    <>
+      <Box className={classes.titleBoxMobile}>
+        <Typography variant="h3">Categories</Typography>
+      </Box>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {categories.map((category) => (
+          <Grid key={category.id} item xs={12} sm={6}>
+            <StoreCategory {...category} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default Categories;
+
+const useStyles = makeStyles()(() => ({
+  titleBoxMobile: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "primary.bright",
+  },
+}));
