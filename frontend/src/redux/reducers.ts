f493f86bf48ac9f14a517ded5ea2ce2eae2bc7de
RalfMathes <ralf.mathes@tutanota.com>
2023-05-15 22:49:02 +0200

    Add title reducer to reducers

--- frontend/src/redux/reducers.ts
+++ frontend/src/redux/reducers.ts
@@ -3,12 +3,14 @@ import shoppingCartReducer from "./slices/shoppingCart/shoppingCart";
 import storeItemsReducer from "./slices/storeItems/storeItems";
 import categoriesReducer from "./slices/categories/categories";
 import collectionsReducer from "./slices/collections/collections";
+import titleReducer from "./slices/title/title";
 
 const rootReducer = combineReducers({
   shoppingCartReducer,
   storeItemsReducer,
   categoriesReducer,
   collectionsReducer,
+  titleReducer,
 });
 
 export default rootReducer;
