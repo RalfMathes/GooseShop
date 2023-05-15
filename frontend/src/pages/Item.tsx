6d3d3b66a1fea84074c5186dfae7d86ccd3d4406
RalfMathes <ralf.mathes@tutanota.com>
2023-05-15 22:53:41 +0200

    Use MobileAppBar in Item.tsx

--- frontend/src/pages/Item.tsx
+++ frontend/src/pages/Item.tsx
@@ -4,18 +4,23 @@ import StoreItem from "../components/StoreItem";
 import { makeStyles } from "tss-react/mui";
 import { useParams } from "react-router-dom";
 import useGetItem from "../hooks/useGetItem";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setTitle } from "../redux/slices/title/title";
 
 const Item = () => {
   const { itemId } = useParams();
   const filterId = Number.parseInt(itemId ?? "1");
   const { classes } = useStyles();
   const item = useGetItem(filterId);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setTitle(item?.name));
+  }, []);
 
   return (
     <>
-      <Box className={classes.titleBoxMobile}>
-        <Typography variant="h3">{item?.name}</Typography>
-      </Box>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid key={item?.id} item xs={12} sm={6}>
           <StoreItem
@@ -39,10 +44,4 @@ const Item = () => {
 
 export default Item;
 
-const useStyles = makeStyles()(() => ({
-  titleBoxMobile: {
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "primary.bright",
-  },
-}));
+const useStyles = makeStyles()(() => ({}));
