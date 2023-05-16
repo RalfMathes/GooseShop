c9243992af51cd2dab1a89384f6438f3495ff967
RalfMathes <ralf.mathes@tutanota.com>
2023-05-16 21:22:42 +0200

    Remove Container from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Container from "@mui/material/Container";
 import { Route, Routes } from "react-router-dom";
 import MobileSpacer from "./components/MobileSpacer";
-import MobileTopBar from "./components/MobileTopBar";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Categories from "./pages/Categories";
@@ -19,19 +17,16 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Navbar />
-        <MobileTopBar />
-        <Container>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/collection" element={<Collections />} />
-            <Route path="/collection/:collectionId" element={<Collection />} />
-            <Route path="/category" element={<Categories />} />
-            <Route path="/category/:categoryId" element={<Category />} />
-            <Route path="/item/:itemId" element={<Item />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-          <MobileSpacer />
-        </Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collections />} />
+          <Route path="/collection/:collectionId" element={<Collection />} />
+          <Route path="/category" element={<Categories />} />
+          <Route path="/category/:categoryId" element={<Category />} />
+          <Route path="/item/:itemId" element={<Item />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <MobileSpacer />
       </ThemeProvider>
     </>
   );
