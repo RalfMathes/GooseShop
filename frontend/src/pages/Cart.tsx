0917327b1dec8fa15c38156c56b54fca049efb28
RalfMathes <ralf.mathes@tutanota.com>
2023-05-16 21:31:16 +0200

    Add total to Cart page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,12 +1,14 @@
-import { Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { makeStyles } from "tss-react/mui";
 import CartItem from "../components/CartItem";
 import useGetBreakpointBool from "../hooks/useGetBreakpointBool";
+import { setTotal } from "../redux/slices/shoppingCart/shoppingCart";
 import { setTitle } from "../redux/slices/title/title";
 import { RootState } from "../redux/store";
 import { CartItemProps } from "../types/CartItemProps";
+import formatCurrency from "../utilities/formatCurrency";
 
 const Cart = () => {
   const { classes } = useStyles();
@@ -14,6 +16,9 @@ const Cart = () => {
     (state) => state.shoppingCartReducer.items
   );
   const isMobileView = useGetBreakpointBool();
+  const total = useSelector<RootState, number>(
+    (state) => state.shoppingCartReducer.total
+  );
 
   const dispatch = useDispatch();
 
@@ -21,19 +26,43 @@ const Cart = () => {
     dispatch(setTitle("Cart"));
   }, []);
 
+  useEffect(() => {
+    dispatch(setTotal());
+  }, [items]);
+
   return (
     <>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {items.map((item) => (
-          <Grid key={item.id} item xs={12} sm={12}>
-            <CartItem {...item} />
-          </Grid>
-        ))}
-      </Grid>
+      <Container className={classes.cartContainer}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {items.map((item, index) => (
+            <Grid key={index} item xs={12} sm={12}>
+              <CartItem {...item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <div className={classes.textColoDiv}>
+        <Container>
+          <Typography className={classes.totalText} variant="h4">
+            Total: {formatCurrency(total)}
+          </Typography>
+        </Container>
+      </div>
     </>
   );
 };
 
 export default Cart;
 
-const useStyles = makeStyles()(() => ({}));
+const useStyles = makeStyles()(() => ({
+  cartContainer: {
+    paddingTop: 15,
+  },
+  textColoDiv: {
+    backgroundColor: "rgba(251, 139, 23, 0.4)",
+  },
+  totalText: {
+    padding: "20px 20px 20px 0",
+    textAlign: "end",
+  },
+}));
