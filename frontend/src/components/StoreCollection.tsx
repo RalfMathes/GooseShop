c284fff34fa9c0e72d07d8d82a62038804b6d096
RalfMathes <ralf.mathes@tutanota.com>
2023-05-16 21:39:26 +0200

    Overhaul StoreCollection styling

--- frontend/src/components/StoreCollection.tsx
+++ frontend/src/components/StoreCollection.tsx
@@ -1,37 +1,49 @@
-import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { makeStyles } from "tss-react/mui";
 import useGetBreakpointBool from "../hooks/useGetBreakpointBool";
 import { CollectionProps } from "../types/CollectionProps";
 
-const StoreCollection = ({ id, name, imgUrl }: CollectionProps) => {
+const StoreCollection = ({
+  id,
+  name,
+  description,
+  imgUrl,
+}: CollectionProps) => {
   const { classes } = useStyles();
   const isMobileView = useGetBreakpointBool();
+  const idIsEven = id % 2 === 0;
+  const renderMediaLeft = !isMobileView && idIsEven;
 
   return (
     <Link className={classes.navMenuLink} to={`/collection/${id}`}>
-      <Card
-        className={
-          isMobileView
-            ? classes.collectionCardMobile
-            : classes.collectionCardDesktop
-        }
-      >
-        <CardMedia
-          className={classes.collectionCardMedia}
-          image={imgUrl}
-          title={name}
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h5"
-            className={classes.contentTitle}
-          >
-            {name}
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-        </CardContent>
+      <Card className={classes.collectionCardDesktop}>
+        <div className={classes.storeCollectionWrapper}>
+          {renderMediaLeft ? null : (
+            <CardMedia
+              className={classes.collectionCardMedia}
+              image={imgUrl}
+              title={name}
+            />
+          )}
+          <CardContent className={classes.collectionCardContent}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              className={classes.contentTitle}
+            >
+              The {name} collection
+            </Typography>
+            <Typography variant="body1">{description}</Typography>
+          </CardContent>
+          {renderMediaLeft ? (
+            <CardMedia
+              className={classes.collectionCardMedia}
+              image={imgUrl}
+              title={name}
+            />
+          ) : null}
+        </div>
       </Card>
     </Link>
   );
@@ -39,19 +51,38 @@ const StoreCollection = ({ id, name, imgUrl }: CollectionProps) => {
 
 export default StoreCollection;
 
-const useStyles = makeStyles()(() => ({
+const useStyles = makeStyles()((theme) => ({
   collectionCardDesktop: {
-    width: 400,
+    width: "100%",
+    marginTop: 50,
+    flexGrow: 1,
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      flexDirection: "row",
+    },
+  },
+  collectionCardContent: {
+    [theme.breakpoints.up("sm")]: {
+      width: "60%",
+    },
   },
   collectionCardMedia: {
-    height: 140,
+    height: 220,
+    flexGrow: 1,
+    [theme.breakpoints.up("sm")]: {
+      width: "40%",
+      height: 400,
+    },
   },
   collectionCardMobile: {
     width: 200,
+    marginBottom: 20,
+    flexGrow: 1,
   },
   contentTitle: {
     display: "flex",
     justifyContent: "center",
+    // backgroundColor: "#fb8b17",
   },
   flexBoxCenter: {
     alignItems: "center",
@@ -60,5 +91,11 @@ const useStyles = makeStyles()(() => ({
   navMenuLink: {
     color: "black",
     textDecoration: "none",
+  },
+  storeCollectionWrapper: {
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+      flexDirection: "row",
+    },
   },
 }));
