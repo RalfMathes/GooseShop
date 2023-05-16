84882752259a67896482617ecd8e3c1febe3e176
RalfMathes <ralf.mathes@tutanota.com>
2023-05-16 21:42:35 +0200

    afix useGetItem to use correct type

--- frontend/src/hooks/useGetItem.ts
+++ frontend/src/hooks/useGetItem.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchStoreItems } from "../redux/slices/storeItems/storeItems";
 import { AppDispatch, RootState } from "../redux/store";
-import { CollectionProps } from "../types/CollectionProps";
+import { StoreItemProps } from "../types/StoreItemProps";
 
 const useGetItems = (filterId: number) => {
   const appDispatch = useDispatch<AppDispatch>();
@@ -10,12 +10,12 @@ const useGetItems = (filterId: number) => {
     appDispatch(fetchStoreItems());
   }, []);
 
-  const unfilteredCollections = useSelector<RootState, CollectionProps[]>(
+  const unfilteredCollections = useSelector<RootState, StoreItemProps[]>(
     (state) => state.storeItemsReducer.items
   );
 
   return unfilteredCollections.find(
-    (item: CollectionProps) => item.id == filterId
+    (item: StoreItemProps) => item.id == filterId
   );
 };
 
