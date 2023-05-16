f17752737c0d514d95a537e2686adf0d99a18d30
RalfMathes <ralf.mathes@tutanota.com>
2023-05-16 21:45:19 +0200

    Massively overhaul Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import { Container, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { makeStyles } from "tss-react/mui";
 import StoreCategory from "../components/StoreCategory";
 import StoreCollection from "../components/StoreCollection";
+import useGetBreakpointBool from "../hooks/useGetBreakpointBool";
 import useGetCategories from "../hooks/useGetCategories";
 import useGetCollections from "../hooks/useGetCollections";
 import { fetchCategories } from "../redux/slices/categories/categories";
@@ -17,6 +23,7 @@ const Home = () => {
   const categories = useGetCategories();
   const appDispatch = useDispatch<AppDispatch>();
   const dispatch = useDispatch();
+  const isMobileView = useGetBreakpointBool();
 
   useEffect(() => {
     appDispatch(fetchCategories());
@@ -26,8 +33,43 @@ const Home = () => {
 
   return (
     <>
-      <div>
-        <Typography variant="h4" className={classes.contentTitle}>
+      <img className={classes.hero} src="/static/hero.png" />
+      <Container>
+        <Grid className={classes.iconGrid} container spacing={2}>
+          <Grid className={classes.iconGridItem} item xs={6} sm={3}>
+            <LocalShippingIcon
+              className={classes.spacedIcon}
+              fontSize="large"
+            />
+            <Typography className={classes.iconCaption} variant="body2">
+              Fast and reliable delivery
+            </Typography>
+          </Grid>
+          <Grid className={classes.iconGridItem} item xs={6} sm={3}>
+            <ThumbUpIcon className={classes.spacedIcon} fontSize="large" />
+            <Typography className={classes.iconCaption} variant="body2">
+              Approved by geese around the world
+            </Typography>
+          </Grid>
+          <Grid className={classes.iconGridItem} item xs={6} sm={3}>
+            <ContactSupportIcon
+              className={classes.spacedIcon}
+              fontSize="large"
+            />
+            <Typography className={classes.iconCaption} variant="body2">
+              Friendly and fast support
+            </Typography>
+          </Grid>
+          <Grid className={classes.iconGridItem} item xs={6} sm={3}>
+            <FavoriteIcon className={classes.spacedIcon} fontSize="large" />
+            <Typography className={classes.iconCaption} variant="body2">
+              High quality products
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+      <div className={classes.categoriesRoot}>
+        <Typography variant="h4" className={classes.categoryTitle}>
           Categories
         </Typography>
         <div className={classes.scrollGrid}>
@@ -36,15 +78,15 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <div>
-        <Typography variant="h4" className={classes.contentTitle}>
+      <div className={classes.collectionsRoot}>
+        <Typography variant="h4" className={classes.collectionTitle}>
           Collections
         </Typography>
-        <div className={classes.scrollGrid}>
+        <Container>
           {collections.map((collection) => (
             <StoreCollection key={collection.id} {...collection} />
           ))}
-        </div>
+        </Container>
       </div>
     </>
   );
@@ -52,14 +94,58 @@ const Home = () => {
 
 export default Home;
 
-const useStyles = makeStyles()(() => ({
+const useStyles = makeStyles()((theme) => ({
+  hero: {
+    width: "100%",
+    height: "400",
+    backgroundSize: "cover",
+    marginTop: -70,
+    marginBottom: 70,
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 0,
+      marginBottom: 40,
+    },
+  },
   borderDiv: {
     border: 2,
     borderStyle: "solid",
   },
-  contentTitle: {
+  categoriesRoot: {
+    backgroundColor: "rgba(251, 139, 23, 0.4)",
+    padding: "20px 0px 40px 0px",
+  },
+  collectionsRoot: {
+    padding: "20px 0px 20px 0px",
+  },
+  categoryTitle: {
     display: "flex",
     justifyContent: "center",
+    alignContent: "center",
+    marginBottom: 20,
+  },
+  collectionTitle: {
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    marginBottom: -20,
+  },
+  iconCaption: {
+    margin: "0 30px 0 30px",
+  },
+  iconGrid: {
+    [theme.breakpoints.down("sm")]: {
+      paddingBottom: 30,
+    },
+  },
+  iconGridItem: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "column",
+    textAlign: "center",
+    [theme.breakpoints.up("sm")]: {
+      paddingBottom: 40,
+    },
   },
   scrollGrid: {
     display: "flex",
@@ -67,6 +153,11 @@ const useStyles = makeStyles()(() => ({
     gap: 20,
     overflowX: "auto",
     overflowY: "hidden",
+    scrollbarColor: "#fb8b17",
     whiteSpace: "nowrap",
+    padding: "0 20px 0 20px",
+  },
+  spacedIcon: {
+    marginBottom: 20,
   },
 }));
