42118b59c334b65df261f966e2e8a34a14adb119
RalfMathes <ralf.mathes@tutanota.com>
2023-05-17 15:57:03 +0200

    Remove deprecated links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import MobileSpacer from "./components/MobileSpacer";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
-import Categories from "./pages/Categories";
 import Category from "./pages/Category";
 import Collection from "./pages/Collection";
-import Collections from "./pages/Collections";
 import Home from "./pages/Home";
 import Item from "./pages/Item";
 import theme from "./theme";
@@ -19,9 +17,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/collection" element={<Collections />} />
           <Route path="/collection/:collectionId" element={<Collection />} />
-          <Route path="/category" element={<Categories />} />
           <Route path="/category/:categoryId" element={<Category />} />
           <Route path="/item/:itemId" element={<Item />} />
           <Route path="/cart" element={<Cart />} />
