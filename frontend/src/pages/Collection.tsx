a412160e9e8e3da626fb7009830bb480c7e238a6
RalfMathes <ralf.mathes@tutanota.com>
2023-05-17 15:13:10 +0200

    Add hero and Title to collection

--- frontend/src/pages/Collection.tsx
+++ frontend/src/pages/Collection.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -41,27 +41,59 @@ const Collection = () => {
 
   return (
     <>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        {visibleItems.map((item) => (
-          <Grid key={item.id} item xs={12} sm={6}>
-            <StoreItem
-              id={item.id}
-              categoryId={item.categoryId}
-              collectionId={item.collectionId}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              imgUrl={item.imgUrl}
-              tags={item.tags}
-              onTagClick={storeItemCallback}
-            />
-          </Grid>
-        ))}
-      </Grid>
+      <div className={classes.centerDiv}>
+        <img className={classes.hero} src={collection?.imgUrl} />
+      </div>
+      <Typography variant="h3" className={classes.centeredText}>
+        The {collection?.name} collection
+      </Typography>
+      <Container>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {visibleItems.map((item) => (
+            <Grid key={item.id} item xs={12} sm={6}>
+              <StoreItem
+                id={item.id}
+                categoryId={item.categoryId}
+                collectionId={item.collectionId}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                imgUrl={item.imgUrl}
+                tags={item.tags}
+                onTagClick={storeItemCallback}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 };
 
 export default Collection;
 
-const useStyles = makeStyles()(() => ({}));
+const useStyles = makeStyles()((theme) => ({
+  centerDiv: {
+    height: "50%",
+    display: "flex",
+    justifyContent: "center",
+    position: "relative",
+  },
+  centeredText: {
+    textAlign: "center",
+    marginBottom: 70,
+  },
+  hero: {
+    width: "100%",
+    position: "relative",
+    maxWidth: "1200px",
+    display: "flex",
+    alignItems: "center",
+    marginTop: -70,
+    marginBottom: 70,
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 0,
+      marginBottom: 40,
+    },
+  },
+}));
