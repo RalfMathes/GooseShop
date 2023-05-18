b7f4c33fa688e2f992ef54d4983f94d87151484f
RalfMathes <ralf.mathes@tutanota.com>
2023-05-18 19:56:54 +0200

    Replace equivalent elements with AddToCartCluster

--- frontend/src/components/StoreItem.tsx
+++ frontend/src/components/StoreItem.tsx
@@ -1,8 +1,5 @@
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
 import {
   Box,
-  Button,
   Card,
   CardContent,
   CardMedia,
@@ -10,104 +7,44 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import { useDispatch } from "react-redux";
 import { makeStyles } from "tss-react/mui";
 
 import { Link } from "react-router-dom";
-import useGetItemCount from "../hooks/useGetItemCount";
-import {
-  decreaseItem,
-  increaseItem,
-  removeItem,
-} from "../redux/slices/shoppingCart/shoppingCart";
 import { StoreItemProps } from "../types/StoreItemProps";
 import formatCurrency from "../utilities/formatCurrency";
+import AddToCartCluster from "./AddToCartCluster";
 
-const StoreItem = ({
-  id,
-  name,
-  description,
-  price,
-  imgUrl,
-  tags,
-  onTagClick,
-}: StoreItemProps) => {
-  const itemCount = useGetItemCount(id);
-
+const StoreItem = ({ ...item }: StoreItemProps) => {
   const { classes } = useStyles();
-  const dispatch = useDispatch();
-
-  const noItemsAdded = itemCount === 0;
-
-  const handleIncreaseItem = () => {
-    dispatch(increaseItem([id, price]));
-  };
-
-  const handleDecreaseItem = () => {
-    dispatch(decreaseItem(id));
-  };
-
-  const handleRemoveItem = () => {
-    dispatch(removeItem(id));
-  };
 
   return (
     <Card variant="elevation" className={classes.storeItemCard}>
-      <Link to={`/item/${id}`}>
-        <CardMedia className={classes.cardMedia} image={imgUrl} title={name} />
+      <Link to={`/item/${item.id}`}>
+        <CardMedia
+          className={classes.cardMedia}
+          image={item.imgUrl}
+          title={item.name}
+        />
       </Link>
       <CardContent>
         <Typography gutterBottom variant="h5">
-          {name}
+          {item.name}
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
         <Typography variant="body2" color="text.secondary">
-          {formatCurrency(price)}
+          {formatCurrency(item.price)}
         </Typography>
       </CardContent>
-      {noItemsAdded ? (
-        <Box className={classes.flexBoxCenter}>
-          <Button variant="contained" onClick={handleIncreaseItem}>
-            Add to Cart
-          </Button>
-        </Box>
-      ) : (
-        <>
-          <Box className={classes.flexBoxCenter}>
-            <Button
-              size="small"
-              variant="contained"
-              onClick={handleDecreaseItem}
-            >
-              <RemoveIcon />
-            </Button>
-            <Typography className={classes.cartCountText}>
-              {itemCount} in cart
-            </Typography>
-            <Button
-              size="small"
-              variant="contained"
-              onClick={handleIncreaseItem}
-            >
-              <AddIcon />
-            </Button>
-          </Box>
-          <Box className={classes.flexBoxCenter}>
-            <Button size="small" variant="contained" onClick={handleRemoveItem}>
-              Remove Item
-            </Button>
-          </Box>
-        </>
-      )}
+      <AddToCartCluster {...item} />
       <Stack className={classes.tagStack} direction="row" spacing={2}>
-        {tags.map((tag: string, index: number) => (
+        {item.tags.map((tag: string, index: number) => (
           <Chip
             key={index}
             className={classes.tagChip}
             color="primary"
             size="small"
             label={tag}
-            onClick={(event) => onTagClick(event, tag)}
+            onClick={(event) => item.onTagClick(event, tag)}
           />
         ))}
       </Stack>
