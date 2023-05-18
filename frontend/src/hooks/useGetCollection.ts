962d0e4a894b8455f3d579bf3edc29d20075cc91
RalfMathes <ralf.mathes@tutanota.com>
2023-05-18 17:13:31 +0200

    Replace collections selector call with respective hook

--- frontend/src/hooks/useGetCollection.ts
+++ frontend/src/hooks/useGetCollection.ts
@@ -1,11 +1,8 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
 import { CollectionProps } from "../types/CollectionProps";
+import useGetCollections from "./useGetCollections";
 
 const useGetCollection = (filterId: number) => {
-  const unfilteredCollections = useSelector<RootState, CollectionProps[]>(
-    (state) => state.collectionsReducer.collections
-  );
+  const unfilteredCollections = useGetCollections();
 
   return unfilteredCollections.find(
     (collection: CollectionProps) => collection.id == filterId
