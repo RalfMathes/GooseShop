4ce312fcaf16f4aafe2027332b39c2234181ce04
RalfMathes <ralf.mathes@tutanota.com>
2023-05-18 16:56:56 +0200

    Replace catrgories selector call with respective hook

--- frontend/src/hooks/useGetCategory.ts
+++ frontend/src/hooks/useGetCategory.ts
@@ -1,11 +1,8 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
 import { CategoryProps } from "../types/CategoryProps";
+import useGetCategories from "./useGetCategories";
 
 const useGetCategory = (filterId: number) => {
-  const unfilteredCategories = useSelector<RootState, CategoryProps[]>(
-    (state) => state.categoriesReducer.categories
-  );
+  const unfilteredCategories = useGetCategories();
 
   return unfilteredCategories.find(
     (category: CategoryProps) => category.id == filterId
